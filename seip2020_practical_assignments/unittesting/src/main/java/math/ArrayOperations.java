2474f4547814ec6af23cdb63a09c5eb3bc642b23
despoinasr <despoinasr@gmail.com>
2020-04-23 21:06:25 +0300

    add javadoc

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
@@ -1,8 +1,27 @@
 package math;
 
+import java.io.IOException;
+
 import io.FileIO;
 
+/**
+* The ArrayOpearations provides the implementation of the search of primes in a file
+*
+* @author  Despoina Sfetsori
+* @version 1.0
+* @since   2020-04-23
+*/
+
 public class ArrayOperations {
+	
+	/**
+	 * Reads a file that contains numbers line by line 
+	 * and returns an array of the prime integers found in the file.
+	 * @param fileIo is an object of the FileIO class
+	 * @param filepath the file that contains the numbers
+	 * @param myMath is an object of the MyMath class
+	 * @return an array of numbers
+	 */
 	
 	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
 		int[] array = fileIo.readFile(filepath);
--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -1,6 +1,23 @@
 package math;
 
+/**
+* The MyMath provides the calculation of the factorial of an integer and the checks if
+* a number is prime
+*
+* @author  Despoina Sfetsori
+* @version 1.0
+* @since   2020-04-23
+*/
+
 public class MyMath {
+	
+	/**
+	 * Reads an integer and returns its factorial
+	 * @param n an integer
+	 * @return the factorial
+	 * @exception IllegalArgumentException when the given file integer does not belong
+	 * in the interval (0,12)
+	 */
 	
 	public int factorial (int n) {
 		int f = n;
@@ -15,6 +32,13 @@ public class MyMath {
 				throw new IllegalArgumentException("n should be 0 > n > 12");
 		}
 	}
+	
+	/**
+	 * Reads an integer and returns true if the number is prime, false otherwise
+	 * @param n an integer
+	 * @return true of the number is prime, or false
+	 * @exception IllegalArgumentException when the given file integer is less than 2
+	 */
 	
 	public  boolean isPrime(int n) {
 		if (n >= 2) {
