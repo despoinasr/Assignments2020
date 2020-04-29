6522d56ad8ce3b306eee70a207b410a77bb4aad2
despoinasr <despoinasr@gmail.com>
2020-04-29 17:25:42 +0300

    fix array in ArrayOperations class

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
@@ -37,7 +37,7 @@ public class ArrayOperations {
 		int[] primeArray = new int[list.size()];
 		
 		for (int i = 0; i < list.size(); i++) {
-			array[i] = list.get(i);
+			primeArray[i] = list.get(i);
 		}
 		return primeArray;
 	}
