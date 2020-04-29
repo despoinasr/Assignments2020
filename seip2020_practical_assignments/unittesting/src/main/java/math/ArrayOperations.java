e05c8512387a0ba6ae1d1b250918ad52def06f03
despoinasr <despoinasr@gmail.com>
2020-04-29 17:14:10 +0300

    use list in findPrimesInFile

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
@@ -1,6 +1,8 @@
 package math;
 
 
+import java.util.ArrayList;
+
 import io.FileIO;
 
 
@@ -25,19 +27,17 @@ public class ArrayOperations {
 	
 	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
 		int[] array = fileIo.readFile(filepath);
-		int x = 0;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
 		for (int i = 0; i < array.length; i++) {
 			if (myMath.isPrime(array[i])) {
-				x++;
+				list.add(array[i]);
 			}
 		}
-		int[] primeArray = new int[x];
-		x = 0;
-		for (int i = 0; i < array.length; i++) {
-			if (myMath.isPrime(array[i])) {
-				primeArray[x] = array[i];
-				x++;
-			}
+		int[] primeArray = new int[list.size()];
+		
+		for (int i = 0; i < list.size(); i++) {
+			array[i] = list.get(i);
 		}
 		return primeArray;
 	}
